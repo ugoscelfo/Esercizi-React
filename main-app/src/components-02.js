34a46935246434fc056f03dca647428fc944dcb4
Ugo Scelfo <ugoscelfo92@gmail.com>
2022-11-04 10:47:11 +0100

    updated components-02

--- main-app/src/components-02.js
+++ main-app/src/components-02.js
@@ -1,5 +1,9 @@
-class HelloWorld extends React.Component {
+import React from "react";
+
+export class HelloWorld extends React.Component {
   render() {
-    return <h1>Hello, World!</h1>
+    return (
+    <h1>Hello, World!</h1>
+    );
   }
 }
