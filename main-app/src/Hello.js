b6cd90d1210b0629ca194ebd253d065ab65a0a67
Ugo Scelfo <ugoscelfo92@gmail.com>
2022-11-05 17:41:21 +0100

    esercizio components-06

--- main-app/src/Hello.js
+++ main-app/src/Hello.js
@@ -3,7 +3,11 @@ import React from "react";
 class Hello extends React.Component {
   render() {
     return (
-    <h1>Hello, World!</h1>
+      <div>
+        <h1>Hello, World!</h1>
+        <p>What a beautiful day!</p>
+      </div>
+    
     );
   }
 }
