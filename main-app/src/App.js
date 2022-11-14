cf4348024eca3570ee5b6f465b40c59220bbb0dd
Ugo Scelfo <ugoscelfo92@gmail.com>
2022-11-15 00:55:45 +0100

    esercizio forms 02

--- main-app/src/App.js
+++ main-app/src/App.js
@@ -1,20 +1,13 @@
 import React from 'react';
-import Hello from './Hello';
-import Welcome from './Welcome';
-import Counter from './Counter';
-import ClickTracker from './ClickTracker';
 import InteractiveWelcome from './InteractiveWelcome';
+import Login from './Login';
 
  class App extends React.Component {
   render() {
     return (
       <div>
-        <Hello />
-        <Welcome name="John" />
-        <Counter initialValue={0} incrementBy={1}/>
-        <br />
-        <ClickTracker />
         <InteractiveWelcome name={this.props.name}/>
+        <Login />
       </div>
     )
   }
