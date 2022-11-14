5cd83822a1c3eeb22d7c3b54d64d3cfb7c647515
Ugo Scelfo <ugoscelfo92@gmail.com>
2022-11-14 10:12:31 +0100

    modifiche props 04

--- main-app/src/Welcome.js
+++ main-app/src/Welcome.js
@@ -1,12 +1,10 @@
 import React from 'react'
 
-const name = "ugo";
-
 class Welcome extends React.Component {
   render() {
     return (
       <div>
-        <p>Welcome, <strong>{name}!</strong></p>
+        <p>Welcome, {this.props.name}</p>
         <p>Your age is {this.props.age}</p>
       </div>
     )
