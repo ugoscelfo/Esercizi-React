bfd0c151deb38c2be6ce52f86de7335d84f54532
Ugo Scelfo <ugoscelfo92@gmail.com>
2022-11-21 21:30:21 +0100

    esercizio function components 01

--- main-app/src/App.js
+++ main-app/src/App.js
@@ -1,31 +1,14 @@
 import React from 'react';
 import Container from './Container';
-import DisplayLanguage from './DisplayLanguage';
-import { LanguageContext } from './LanguageContext';
+import Welcome from './Welcome';
 
- class App extends React.Component {
-  state = {
-    language: "en"
-  }
-
-  handlanguageChange = (event) => {
-    this.setState({
-      language: event.target.value
-    })
-  }
-
+class App extends React.Component {
   render() {
     return (
       <div>
-        <select value={this.state.language} onChange={this.handlanguageChange}>
-          <option value="en">English</option>
-          <option value="it">Italiano</option>
-        </select>
-        <LanguageContext.Provider value={this.state.language}>
-          <Container title="My app">
-            <DisplayLanguage />
-          </Container>
-        </LanguageContext.Provider>
+        <Container title="My app">
+          <Welcome name={"ugo"}/>
+        </Container>
       </div>
     )
   }
