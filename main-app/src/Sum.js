6bbffe62a815cb39edb8d505cebc16860fcc8bb8
Ugo Scelfo <ugoscelfo92@gmail.com>
2022-11-22 10:05:47 +0100

    esercizio function components 03

--- main-app/src/Sum.js
+++ main-app/src/Sum.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
-export function Sum(props) {
-  const numbers = [1, 2, 3, 4]
+export function Sum({ numbers = [1, 2, 3, 4] }) {
+  // default value can be assigned directly in the parameter
   const sum = numbers.reduce((add, number) => {
     return add + number;
   }, 0);
