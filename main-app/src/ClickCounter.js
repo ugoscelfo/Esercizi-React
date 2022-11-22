64a4d43d35127bc127ca5a3d433223171f70ffe9
Ugo Scelfo <ugoscelfo92@gmail.com>
2022-11-22 17:58:11 +0100

    esercizio useeffect 01

--- main-app/src/ClickCounter.js
+++ main-app/src/ClickCounter.js
@@ -1,7 +1,15 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 export function ClickCounter({ initialValue = 0 }) {
   const [count, setCount] = useState(initialValue);
+
+  function onCounterChange() {
+    console.log(`Counting ${count}`);
+  }
+
+  //first parameter is the function we want to be triggered
+  //second parameter is a dependency array
+  useEffect(onCounterChange, [count]);
 
   function counterIncrement() {
     setCount(count => count + 1)
