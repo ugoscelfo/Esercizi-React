8fa4bc72717227ba470c00ce7ab02e1786ffda8d
Ugo Scelfo <ugoscelfo92@gmail.com>
2022-11-28 10:50:20 +0100

    esercizio custom hooks 04

--- main-app/src/GithubUser.js
+++ main-app/src/GithubUser.js
@@ -2,30 +2,48 @@ import React, {useEffect, useState} from 'react';
 
 // Custom Hook
 function useGithubUser(username) {
-  const [data, setData] = useState(null)
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  async function fetchGithubUser(username) {
+    setLoading(true)
+    setError(null)
+
+    try {
+      const response = await fetch(`https://api.github.com/users/${username}`)
+      const json = await response.json()
+      setData(json)
+      if(response.status !== 200) {
+        setError(new Error())
+      }
+    } catch(error) {
+      setError(error)
+      setData(null)
+    } finally {
+      setLoading(false)
+    }
+  }
 
   useEffect(() => {
-    fetch(`https://api.github.com/users/${username}`)
-      .then(response => {
-        return response.json()
-      })
-      .then(json => {
-        console.log(json);
-        setData(json)
-      })
+    fetchGithubUser(username)
   }, [username])
 
   return{
-    data
+    data,
+    loading,
+    error
   }
 }
 
 // Component
 export function GithubUser({ username }) {
-  const {data} = useGithubUser(username)
+  const {data, loading, error} = useGithubUser(username)
 
   return(
     <div>
+      {loading && <h1>Loading</h1>}
+      {error && <h1>Something is wrong</h1>}
       {data && <h1>Name: {data.name}</h1>}
       {data && <h1>Location: {data.location}</h1>}
     </div>
