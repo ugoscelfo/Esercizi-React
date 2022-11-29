e435339cb88713409237a3e0d7498a706a55ef81
Ugo Scelfo <ugoscelfo92@gmail.com>
2022-11-29 18:12:29 +0100

    esercizio react router-04

--- main-app/src/App.js
+++ main-app/src/App.js
@@ -1,16 +1,31 @@
 import React from 'react';
-import {Route, Routes, useParams} from 'react-router-dom';
+import {Route, Routes} from 'react-router-dom';
 import {Counter} from './Counter';
 import { ShowGithubUser } from './ShowGithubUser';
+import { Link } from 'react-router-dom';
+import Container from './Container';
 
 import Welcome from './Welcome';
 
 export default function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Welcome name={"ugo"}/>} />
-      <Route path="/counter" element={<Counter />} />
-      <Route path="users/:username" element={<ShowGithubUser />} />
-    </Routes>
+    <div>
+      <Routes>
+        <Route path="/welcome" element={<Welcome name={"ugo"}/>} />
+        <Route path="/counter" element={<Counter />} />
+        <Route path="users/:username" element={<ShowGithubUser />} />
+      </Routes>
+      <Container title="My app">
+        <ul>
+        <li><Link to="/welcome">Welcome</Link></li>
+        <br />
+        <li><Link to="/counter">Go to the Counter</Link></li>
+        <br />
+        <li><Link to="users/ugoscelfo">Go to the github user</Link></li>
+        </ul>
+      </Container>
+    </div>
+    
+    
   )
 }
--- main-app/src/Welcome.js
+++ main-app/src/Welcome.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import Age from './Age';
 import { DisplayLanguage } from './DisplayLanguage';
-import { Link } from 'react-router-dom';
-
 
 export default function Welcome(props) {
   return(
@@ -10,9 +8,7 @@ export default function Welcome(props) {
       <DisplayLanguage />
       <p>Welcome, <strong>{props.name}</strong></p>
       <Age age={30} />
-      <Link to="/counter">Go to the Counter</Link>
-      <br />
-      <Link to="users/ugoscelfo">Go to the github user</Link>
+      
     </div>
   )
 }
