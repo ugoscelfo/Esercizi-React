612cca4d065de567355d0fa62d8ef1de758a9ee8
Ugo Scelfo <ugoscelfo92@gmail.com>
2022-11-29 11:28:39 +0100

    esercizio usecontext

--- main-app/src/App.js
+++ main-app/src/App.js
@@ -1,17 +1,27 @@
-import React from 'react';
-import { ClickCounter } from './ClickCounter';
+import React, { useState } from 'react';
 import Container from './Container';
+import { LanguageContext } from './LanguageContext';
 
 import Welcome from './Welcome';
 
 export default function App() {
+  const [language, setLanguage] = useState("en")
+
+  function handleChangeLanguage(event) {
+    setLanguage(event.target.value)
+  }
 
   return (
     <div>
-      <Container title="My app">
-        <Welcome name={"ugo"}/>
-        <ClickCounter />
-      </Container>
+      <select value={language} onChange={handleChangeLanguage}>
+        <option value="en">EN</option>
+        <option value="it">IT</option>
+      </select>
+      <LanguageContext.Provider value={language}>
+        <Container title="My app">
+          <Welcome name={"ugo"}/>
+        </Container>
+      </LanguageContext.Provider>
     </div>
   )
 }
--- main-app/src/Welcome.js
+++ main-app/src/Welcome.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Age from './Age';
+import { DisplayLanguage } from './DisplayLanguage';
 
 
 export default function Welcome(props) {
   return(
     <div className='welcome'>
+      <DisplayLanguage />
       <p>Welcome, <strong>{props.name}</strong></p>
       <Age age={30} />
     </div>
