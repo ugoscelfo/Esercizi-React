1c32ca84db2fd690b10425d56da7a7b19e342465
Ugo Scelfo <ugoscelfo92@gmail.com>
2022-11-29 19:34:45 +0100

    esercizio react router-06

--- main-app/src/App.js
+++ main-app/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {Route, Routes} from 'react-router-dom';
 import {Counter} from './Counter';
+import { GithubUserList } from './GithubUserList';
 import { ShowGithubUser } from './ShowGithubUser';
 import { Link } from 'react-router-dom';
 import Container from './Container';
@@ -10,21 +11,24 @@ import Welcome from './Welcome';
 export default function App() {
   return (
     <div>
-      <Routes>
-        <Route path="*" element={<h1>Not Found</h1>}/>
-        <Route path="/welcome" element={<Welcome name={"ugo"}/>} />
-        <Route path="/counter" element={<Counter />} />
-        <Route path="users/:username" element={<ShowGithubUser />} />
-      </Routes>
       <Container title="My app">
         <ul>
         <li><Link to="/welcome">Welcome</Link></li>
         <br />
         <li><Link to="/counter">Go to the Counter</Link></li>
         <br />
-        <li><Link to="users/ugoscelfo">Go to the github user</Link></li>
+        <li><Link to="/users">Github users</Link></li>
         </ul>
       </Container>
+      <Routes>
+        <Route path="*" element={<h1>Not Found</h1>}/>
+        <Route path="/welcome" element={<Welcome name={"ugo"}/>} />
+        <Route path="/counter" element={<Counter />} />
+        <Route path="/users" element={<GithubUserList />} >
+          <Route index element={<p>Please select a user</p>} />
+          <Route path=":username" element={<ShowGithubUser />}/>
+        </Route>
+      </Routes>
     </div>
     
     
